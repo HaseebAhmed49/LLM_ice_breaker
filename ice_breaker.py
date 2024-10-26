1890cdcdf9b25816b1c8d067264ecb3dc399a30c
haseebahmed <haseebahmed02@gmail.com>
2024-10-26 20:37:42 +0100

    LinkedIn Custom Search using LLM

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 import os
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__=='__main__':
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
         given the linkedIn information {information} about a person from I want you to create:
@@ -16,12 +18,17 @@ if __name__=='__main__':
         2. two interesting facts about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/haseebahmed49/"
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
     )
-    res = chain.invoke(input={"information":linkedin_data})
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    chain = summary_prompt_template | llm | StrOutputParser()
+
+    res = chain.invoke(input={"information": linkedin_data})
     print(res)
+
+if __name__=='__main__':
+    load_dotenv()
+    print("Ice Breaker")
+    ice_break_with(name="Eden Marco Udemy")
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -2,7 +2,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
